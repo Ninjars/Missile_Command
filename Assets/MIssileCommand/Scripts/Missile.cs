3e4def8967d0caaed80fc3bbc474c6022170c458
Jez <5053926+Ninjars@users.noreply.github.com>
2020-09-28 19:26:34 +0100

    created missile

---
+++ Assets/MIssileCommand/Scripts/Missile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float thrust = 10;
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void launch(Vector2 target) {
+        var vector = (target - rb.position).normalized;
+        rb.AddForce(vector * thrust, ForceMode2D.Impulse);
+    }
+
+    private void OnDisable() {
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0;
+    }
+}
