e28d93fe394148deb23dbc1177d39b98da81a502
jez <jeremy+github@jeremystevens.net>
2020-09-28 21:12:33 +0100

    implemented most basic missile launch behaviour

--- Assets/Missile Command/Scripts/Missile.cs
+++ Assets/Missile Command/Scripts/Missile.cs
@@ -1,20 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Missile : MonoBehaviour {
+    public float missileLayerZ = 8;
     public float thrust = 10;
-    private Rigidbody2D rb;
-
-    private void Awake() {
-        rb = GetComponent<Rigidbody2D>();
+    private Rigidbody2D _rb;
+    private Rigidbody2D rb {
+        get {
+            if (_rb == null) {
+                _rb = GetComponent<Rigidbody2D>();
+            }
+            return _rb;
+        }
     }
 
-    public void launch(Vector3 position, Vector2 target) {
+    public void launch(Vector2 position, Vector2 target) {
+        Debug.Log($"launch {gameObject.name}: {position} -> {target}");
         rb.position = position;
+        transform.position = new Vector3(rb.position.x, rb.position.y, missileLayerZ);
+        gameObject.SetActive(true);
         var vector = (target - rb.position).normalized;
         rb.AddForce(vector * thrust, ForceMode2D.Impulse);
-        gameObject.SetActive(true);
     }
 
     private void OnDisable() {
