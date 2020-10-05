7dfb85c527a61ac220d00c8763a416807c107013
jez <jeremy+github@jeremystevens.net>
2020-10-05 17:36:18 +0100

    adjusted explosion radius calc to work on an animation curve

--- Assets/Missile Command/Scripts/MissileExplosion.cs
+++ Assets/Missile Command/Scripts/MissileExplosion.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 
 public class MissileExplosion : MonoBehaviour {
     public float maxRadius = 2;
-    public float initialRadius = 0.1f;
-    public float expandDuration = 0.75f;
-    public float lingerDuration = 1f;
+    public float duration = 2f;
+    public AnimationCurve expansion;
     public float layerZ = 6;
 
     private CircleCollider2D _circleCollider;
@@ -33,23 +32,23 @@ public class MissileExplosion : MonoBehaviour {
 
     public void boom(Vector2 position) {
         transform.position = new Vector3(position.x, position.y, layerZ);
-        discShape.Radius = initialRadius;
-        circleCollider.radius = initialRadius;
+        discShape.Radius = 0;
+        circleCollider.radius = 0;
         startTime = Time.time;
         gameObject.SetActive(true);
     }
 
     private void Update() {
         float passedTime = Time.time - startTime;
-        if (passedTime <= expandDuration) {
-            expandRadius(passedTime);
-        } else if (passedTime > lingerDuration + expandDuration) {
+        if (passedTime > duration) {
             gameObject.SetActive(false);
+        } else {
+            expandRadius(passedTime / duration);
         }
     }
 
-    private void expandRadius(float passedTime) {
-        float radius = Mathf.Lerp(initialRadius, maxRadius, Mathf.Clamp01(passedTime / expandDuration));
+    private void expandRadius(float normalisedValue) {
+        float radius = expansion.Evaluate(normalisedValue) * maxRadius;
         discShape.Radius = radius;
         circleCollider.radius = radius;
     }
