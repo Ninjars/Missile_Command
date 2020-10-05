91eacfa39351a61d453ecad16527638310cdf7b1
jez <jeremy+github@jeremystevens.net>
2020-10-05 19:51:17 +0100

    initial implementation of first attack type, the ICBM

---
+++ Assets/Missile Command/Scripts/ICBM.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class ICBM : MonoBehaviour {
+    public float worldSpawnBuffer = 1;
+    public float layerZ = 5;
+    public float detonationRange = 0.5f;
+    public MissileExplosion explosionPrefab;
+
+    private Vector3 targetPosition;
+    private float thrust;
+    private Vector3 thrustVector;
+    private Rigidbody2D _rb;
+    private Rigidbody2D rb {
+        get {
+            if (_rb == null) {
+                _rb = GetComponent<Rigidbody2D>();
+            }
+            return _rb;
+        }
+    }
+
+    public void launch(
+        WorldCoords worldCoords,
+        WeaponData weaponData,
+        int level,
+        Vector2 targetCoords
+    ) {
+        Vector3 spawnPosition = calculateSpawnPosition(worldCoords, targetCoords.x);
+        float deviance = UnityEngine.Random.value * weaponData.accuracy.evaluate(level);
+        if (UnityEngine.Random.value < 0.5f) {
+            deviance = -deviance;
+        }
+        this.targetPosition = new Vector3(targetCoords.x + deviance, targetCoords.y, layerZ);
+        this.thrust = weaponData.primaryAcceleration.evaluate(level);
+        this.thrustVector = (targetPosition - spawnPosition).normalized;
+        transform.position = spawnPosition;
+
+        gameObject.SetActive(true);
+        rb.AddForce(thrustVector * weaponData.primaryImpulse.evaluate(level));
+    }
+
+    private Vector3 calculateSpawnPosition(WorldCoords worldCoords, float targetX) {
+        float x;
+        if (targetX > worldCoords.centerX) {
+            x = worldCoords.worldLeft - worldSpawnBuffer;
+        } else {
+            x = worldCoords.worldRight + worldSpawnBuffer;
+        }
+        return new Vector3(x, worldCoords.worldTop+worldSpawnBuffer, layerZ);
+    }
+
+    private void explode() {
+        gameObject.SetActive(false);
+
+        var explosion = ObjectPoolManager.Instance.getObjectInstance(explosionPrefab.gameObject).GetComponent<MissileExplosion>();
+        explosion.boom(transform.position);
+    }
+
+    private void FixedUpdate() {
+        if (Vector3.Distance(transform.position, targetPosition) < detonationRange) {
+            explode();
+        } else {
+            rb.AddForce(thrustVector * thrust, ForceMode2D.Force);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Debug.DrawLine(
+            transform.position,
+            targetPosition,
+            Color.red
+        );
+    }
+}
