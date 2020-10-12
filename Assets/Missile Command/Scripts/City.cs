a316fdce739847edb669919fc057d810fc442029
jez <jeremy+github@jeremystevens.net>
2020-10-12 23:05:21 +0100

    implemented destroyed and alive city graphics and added an explosion when cities are destroyed

--- Assets/Missile Command/Scripts/City.cs
+++ Assets/Missile Command/Scripts/City.cs
@@ -1,8 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class City : MonoBehaviour {
     
     public bool isDestroyed = false;
+    public GameObject aliveVisuals;
+    public GameObject deadVisuals;
+    public Explosion explosionPrefab;
 
     private ScreenEffectManager _screenEffectManager;
     private ScreenEffectManager screenEffectManager {
@@ -15,14 +19,26 @@ public class City : MonoBehaviour {
     }
     
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log($"OnTriggerEnter2D {gameObject.name} -> {other.gameObject.name} on layer {LayerMask.LayerToName(other.gameObject.layer)}");
         if (!isDestroyed) {
             destroy();
         }
     }
 
     private void destroy() {
+        if (isDestroyed) return;
+        
         isDestroyed = true;
         screenEffectManager.onCityNukeHit(1f);
+        
+        var explosion = ObjectPoolManager.Instance.getObjectInstance(explosionPrefab.gameObject).GetComponent<Explosion>();
+        explosion.boom(transform.position);
+        StartCoroutine(setDeadVisuals());
+    }
+
+    private IEnumerator setDeadVisuals() {
+        yield return new WaitForSeconds(0.2f);
+
+        aliveVisuals.SetActive(false);
+        deadVisuals.SetActive(true);
     }
 }
