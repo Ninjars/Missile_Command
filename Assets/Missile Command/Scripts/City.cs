2dbc97643ad578e39cdd49debac01a6a29f129bc
jez <jeremy+github@jeremystevens.net>
2020-10-13 13:16:28 +0100

    added logic to cities to support concept of evacuation

--- Assets/Missile Command/Scripts/City.cs
+++ Assets/Missile Command/Scripts/City.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
+using Shapes;
 using UnityEngine;
 
 public class City : MonoBehaviour {
-    
     public bool isDestroyed = false;
+    public Color evacuatedColor;
+    public long population { get; private set; }
+    private long evacuationRate;
     public GameObject aliveVisuals;
     public GameObject deadVisuals;
     public Explosion explosionPrefab;
@@ -17,7 +20,28 @@ public class City : MonoBehaviour {
             return _screenEffectManager;
         }
     }
-    
+
+    public void initialise(long population, long evacuationRate) {
+        this.population = population;
+        this.evacuationRate = evacuationRate;
+    }
+
+    public long evacuate() {
+        if (isDestroyed) {
+            return 0;
+
+        } else if (population <= evacuationRate) {
+            var evacCount = population;
+            population = 0;
+            aliveVisuals.GetComponent<Polyline>().Color = evacuatedColor;
+            return evacCount;
+
+        } else {
+            population -= evacuationRate;
+            return evacuationRate;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (!isDestroyed) {
             destroy();
@@ -26,10 +50,10 @@ public class City : MonoBehaviour {
 
     private void destroy() {
         if (isDestroyed) return;
-        
+        population = 0;
         isDestroyed = true;
         screenEffectManager.onCityNukeHit(1f);
-        
+
         var explosion = ObjectPoolManager.Instance.getObjectInstance(explosionPrefab.gameObject).GetComponent<Explosion>();
         explosion.boom(transform.position);
         StartCoroutine(setDeadVisuals());
