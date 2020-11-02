7af4b01a11e24358907e2f7ff9a4bc86c122c1f8
jez <jeremy+github@jeremystevens.net>
2020-11-02 20:14:22 +0000

    rotate missile visuals to face the direction they are travelling

---
+++ Assets/Missile Command/Scripts/FaceDirectionOfTravel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FaceDirectionOfTravel : MonoBehaviour {
+    private Rigidbody2D rb;
+    
+    private void Awake() {
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
+
+    void Update() {
+        transform.rotation = Quaternion.AngleAxis(Vector3.SignedAngle(Vector3.up, rb.velocity, Vector3.back), Vector3.back);
+    }
+}
