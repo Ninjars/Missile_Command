54d0f66037c1fb4cf344094eac56a420cfb5783d
jez <jeremy+github@jeremystevens.net>
2020-11-09 23:11:01 +0000

    initial implementation for spawning harmless bombers

---
+++ Assets/Missile Command/Scripts/Attacks/Bomber.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+
+public class Bomber : MonoBehaviour {
+    public float worldSpawnBuffer = 1;
+    public float layerZ = 6;
+
+    public Explosion explosionPrefab;
+    public TrailSettings trailSettings;
+
+    private float chargeTime;
+    private float evasionSpeed;
+    private float speed;
+    private float shotDeviation;
+    private Vector2 velocity;
+    private Colors colors { get { return Colors.Instance; } }
+
+    private WorldCoords worldCoords;
+    private StateUpdater stateUpdater;
+    private BomberData weaponData;
+    private Func<Vector2> targetProvider;
+
+    private Rigidbody2D _rb;
+    private LinearTrail trail;
+
+    private Rigidbody2D rb {
+        get {
+            if (_rb == null) {
+                _rb = GetComponent<Rigidbody2D>();
+            }
+            return _rb;
+        }
+    }
+
+    public void launch(
+        StateUpdater stateUpdater,
+        WorldCoords worldCoords,
+        BomberData weaponData,
+        float stageProgress,
+        Func<Vector2> targetProvider,
+        float x,
+        float y
+    ) {
+        this.stateUpdater = stateUpdater;
+        this.weaponData = weaponData;
+        this.targetProvider = targetProvider;
+        this.chargeTime = weaponData.chargeTime.evaluate(stageProgress);
+        this.evasionSpeed = weaponData.evasionSpeed.evaluate(stageProgress);
+        this.speed = weaponData.speed.evaluate(stageProgress);
+        
+        velocity = x < worldCoords.centerX 
+                ? new Vector2(speed, 0)
+                : new Vector2(-speed, 0);
+
+        transform.position = new Vector3(x, y, layerZ);
+        gameObject.SetActive(true);
+
+        rb.velocity = velocity;
+        rb.angularVelocity = 0;
+
+        trail = ObjectPoolManager.Instance.getObjectInstance(trailSettings.prefab.gameObject).GetComponent<LinearTrail>();
+        trail.initialise(gameObject, trailSettings, colors.attackTrailColor);
+    }
+
+    private void Update() {
+        if (transform.position.x < worldCoords.worldLeft - 1) {
+            rb.velocity = new Vector2(speed, 0);
+
+        } else if (transform.position.x > worldCoords.worldRight + 1) {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log($"OnCollisionEnter2D {gameObject.name} -> {other.gameObject.name} on layer {LayerMask.LayerToName(other.gameObject.layer)}");
+        explode();
+    }
+
+    private void explode() {
+        gameObject.SetActive(false);
+
+        var explosion = ObjectPoolManager.Instance.getObjectInstance(explosionPrefab.gameObject).GetComponent<Explosion>();
+        explosion.boom(transform.position, colors.attackExplodeColor);
+    }
+
+    private void OnDisable() {
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0;
+        if (trail != null) {
+            trail.onSubjectDisabled();
+            trail = null;
+        }
+    }
+}
---
+++ Assets/Missile Command/Scripts/Levels/BomberData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bomber Data", menuName = "Missile Command/Bomber")]
+public class BomberData : WeaponData {
+    public Bomber weaponPrefab;
+
+    [Tooltip("Number of bombers per spawn, following same flight path.")]
+    public RangeIntData bombersPerWing;
+
+    [Tooltip("Delay between bomb events.")]
+    public RangeData chargeTime;
+    public RangeData shotDeviation;
+
+    [Tooltip("Horizontal speed.")]
+    public RangeData speed;
+
+    [Tooltip("Ability of bomber to avoid explosions.")]
+    public RangeData evasionSpeed;
+}
